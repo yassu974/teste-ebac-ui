ed52c101a4d154c931fad227ad46c5af35fe67d5
Renan Ferreira <renan.ferrer@hotmail.com>
2025-01-07 18:08:38 -0300

    Testes usando massa de dados em lista

--- cypress/e2e/3-loja-ebac/produtos.cy.js
+++ cypress/e2e/3-loja-ebac/produtos.cy.js
@@ -8,21 +8,36 @@ describe('Funcionalidade: Produtos', () => {
     });
 
     it('Deve selecionar um produto da lista', () => {
-        produtosPage.buscarProdutoLista('Beaumont Summit Kit')
+        produtosPage.buscarProdutoLista('Aero Daily Fitness Tee')
         cy.get('#tab-title-description > a').should('contain','Descrição')
     });
 
     it('Deve buscar um produto com sucesso', () => {
-        let produto = 'Zeppelin Yoga Pant'
+        let produto = 'Aether Gym Pant'
         produtosPage.buscarProduto(produto)
         cy.get('.product_title').should('contain', produto)
     });
 
     it('Deve visitar a página do produto', () => {
-        
+        produtosPage.visitarProduto('Aero Daily Fitness Tee')
+        cy.get('.product_title').should('contain', 'Aero Daily Fitness Tee')
     });
     
     it('Deve adicionar produtos ao carrinho', () => {
-        
+        let qtd = 5
+        produtosPage.buscarProduto('Ariel Roll Sleeve Sweatshirt')
+        produtosPage.addProdutoCarrinho('S' , 'Purple' , qtd)
+        cy.get('.woocommerce-message').should('contain' , qtd + ' × “Ariel Roll Sleeve Sweatshirt” foram adicionados no seu carrinho.')
+    });
+
+    it.only('Deve adicionar produtos ao carrinho buscando da massa de dados', () => {
+        cy.fixture('produtos').then(dados => {
+            produtosPage.buscarProduto(dados[0].nomeProduto)
+            produtosPage.addProdutoCarrinho(
+                dados[0].tamanho , 
+                dados[0].cor , 
+                dados[0].quantidade)
+            cy.get('.woocommerce-message').should('contain' , dados[0].nomeProduto)
+        })
     });
 });
--- cypress/support/page-objects/produtos.page.js
+++ cypress/support/page-objects/produtos.page.js
@@ -4,8 +4,9 @@ class ProdutosPage {
         cy.visit('produtos')
     }
 
-    buscarProduto() {
-        // código
+    buscarProduto(nomeProduto) {
+        cy.get('[name="s"]').eq(1).type(nomeProduto)
+        cy.get('.button-search').eq(1).click()
     }
 
     buscarProdutoLista(nomeProduto) {
@@ -14,12 +15,18 @@ class ProdutosPage {
         .click()
     }
 
-    visitarProduto() {
-
+    visitarProduto(nomeProduto) {
+        //cy.visit(`produtos/${nomeProduto}`)
+        const urlFormatada = nomeProduto.replace(/ /g, '-')
+        cy.visit(`produtos/${urlFormatada}`)
     }
 
-    addProdutoCarrinho() {
-
+    addProdutoCarrinho(tamanho, cor, quantidade) {
+        cy.get('.button-variable-item-' + tamanho).click()
+        cy.get(`.button-variable-item-${cor}`).click()
+        cy.get('.input-text').clear().type(quantidade)
+        cy.get('.single_add_to_cart_button').click()
+        cy.get('.woocommerce-message').should('contain' , 'no seu carrinho.')
     }
 }
 
